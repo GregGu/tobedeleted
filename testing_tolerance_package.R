f9c744887af7ce3e9a0ace2faae1ecfacf5f1c6e
Greg <gguranic@umass.edu>
2022-02-03 13:49:19 -0500

    tolerance package example

--- testing_tolerance_package.R
+++ testing_tolerance_package.R
@@ -4,52 +4,40 @@ check_perc <- function(x, up, low) {
   within/length(x)*100
 }
 
-N <- 10000
+# making up some values for mus and dispersions
 mat_death <- list()
+N <- 5
+mus <- runif(N, 2, 100) 
+sizes <- runif(N, 5, 50)
 for (i in 1:N) { 
   mat_death[[i]] <- rnbinom(n = 10000, #samples
-                            size = 5, #true dispersion aka size aka r
-                            mu = 50 #true mean maternal deaths)
+                            size = sizes[i], #true dispersion aka size aka r
+                            mu = mus[i]) #true mean maternal deaths)
 }
 
-hist(mat_death[[1]])
-ci <- tolerance::negbintol.int(
-  x  = 50,
-  n = 5, #this is r aka size aka success
-  m = NULL,
-  alpha = 0.2,
-  P = 0.99,
-  side = 2,
-  method = c("CB")
-)
-ci
+t <- mat_death %>% unlist()
+hist(t, breaks = 50) #makes sense looks uniform if you look at all dif populations created
+
+
+cis <- list()
+for (i in 1:N) { 
+  cis[[i]] <- tolerance::negbintol.int(
+    x  = mus[i],
+    n = sizes[i], #this is r aka size aka success
+    m = NULL,
+    alpha = 0.2,
+    P = 0.99,
+    side = 2,
+    method = c("CB")
+  )
+}
 
 perc <- c()
 for (i in 1:N) {
-  perc <- check_perc(
-    x = mat_death[[3]],
-    up = ci$`2-sided.upper`,
-    low = ci$`2-sided.lower`)
+  perc[i] <- check_perc(
+    x = mat_death[[i]],
+    up = cis[[i]]$`2-sided.upper`,
+    low = cis[[i]]$`2-sided.lower`)
 }
 mean(perc)
 
-
-ci <- tolerance::negbintol.int(
-  x  = 50,
-  n = 5, #this is r aka size aka success
-  m = NULL,
-  alpha = 0.2,
-  P = 0.5,
-  side = 2,
-  method = c("CB")
-)
-ci
-
-perc <- c()
-for (i in 1:N) {
-  perc <- check_perc(
-    x = mat_death[[3]],
-    up = ci$`2-sided.upper`,
-    low = ci$`2-sided.lower`)
-}
-mean(perc)
